5632998afbdc692dd1dda1974cb2d5f53a380a3d
Raisonjosephau <raisonjosephau@gmail.com>
2018-08-24 12:08:02 +0530

    add widgets to the result page

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -13,499 +13,676 @@ import {PagedData} from '../_objects/page-data'
 export class ResultComponent implements OnInit {
   rows = [
     {
-      'id': 'LSGI00',
-      'name': 'Francis Vance',
+      'id': 'SGICS00',
+      'name': 'Nora Goodman',
       'grade': [
-        'A+',
-        'B',
-        'B',
-        'F',
-        'A+',
-        'O',
-        'A+',
-        'D+',
-        'O'
-      ]
-    },
-    {
-      'id': 'LSGI01',
-      'name': 'George Stuart',
-      'grade': [
-        'A+',
-        'C',
-        'O',
-        'O',
-        'B+',
-        'D+',
-        'A',
         'C+',
-        'A'
-      ]
-    },
-    {
-      'id': 'LSGI02',
-      'name': 'Woodard Mccarty',
-      'grade': [
-        'P',
-        'A',
-        'F',
-        'C',
         'B+',
-        'C',
-        'C',
+        'A+',
         'O',
-        'P'
-      ]
-    },
-    {
-      'id': 'LSGI03',
-      'name': 'Rodriguez Mullins',
-      'grade': [
+        'A+',
         'O',
-        'F',
-        'B',
-        'A',
-        'D+',
-        'P',
-        'A',
-        'D+',
+        'A+',
+        'C+',
         'B'
-      ]
+      ],
+      'cgpa': 6.1264
     },
     {
-      'id': 'LSGI04',
-      'name': 'Duran Lane',
+      'id': 'SGICS01',
+      'name': 'Porter Stafford',
       'grade': [
-        'B+',
-        'B+',
-        'B',
-        'F',
-        'B+',
-        'A',
         'C',
-        'P',
-        'B'
-      ]
-    },
-    {
-      'id': 'LSGI05',
-      'name': 'Sherry Vincent',
-      'grade': [
-        'F',
-        'O',
-        'A',
-        'A',
-        'F',
-        'F',
-        'C',
-        'D+',
-        'O'
-      ]
-    },
-    {
-      'id': 'LSGI06',
-      'name': 'Hurst Phelps',
-      'grade': [
-        'O',
-        'D+',
-        'D+',
-        'O',
-        'O',
-        'D+',
-        'B',
-        'C',
-        'C'
-      ]
-    },
-    {
-      'id': 'LSGI07',
-      'name': 'Valeria Ayers',
-      'grade': [
-        'D+',
-        'F',
-        'O',
-        'P',
-        'A',
-        'C+',
-        'D+',
-        'B+',
-        'O'
-      ]
-    },
-    {
-      'id': 'LSGI08',
-      'name': 'Hansen Brennan',
-      'grade': [
-        'B+',
-        'A',
         'A+',
+        'A',
+        'A',
         'F',
-        'P',
-        'P',
-        'P',
-        'C+',
+        'C',
+        'O',
+        'A',
         'A'
-      ]
+      ],
+      'cgpa': 8.6126
     },
     {
-      'id': 'LSGI09',
-      'name': 'Garcia Underwood',
+      'id': 'SGICS02',
+      'name': 'Adele Pruitt',
       'grade': [
-        'A+',
-        'P',
+        'D+',
         'A',
-        'O',
-        'B+',
         'C+',
+        'C',
+        'C+',
+        'O',
+        'A',
+        'A+',
+        'C+'
+      ],
+      'cgpa': 7.3491
+    },
+    {
+      'id': 'SGICS03',
+      'name': 'Craft Brewer',
+      'grade': [
         'F',
-        'B+',
-        'B+'
-      ]
-    },
-    {
-      'id': 'LSGI010',
-      'name': 'Nash Norris',
-      'grade': [
-        'A',
-        'C',
-        'B',
-        'A+',
+        'F',
         'O',
-        'O',
-        'O',
-        'O',
-        'P'
-      ]
-    },
-    {
-      'id': 'LSGI011',
-      'name': 'Eugenia Barker',
-      'grade': [
-        'C+',
-        'B',
         'P',
-        'A+',
         'O',
-        'D+',
-        'A+',
-        'C+',
-        'A'
-      ]
-    },
-    {
-      'id': 'LSGI012',
-      'name': 'Riggs Mitchell',
-      'grade': [
-        'C',
-        'D+',
-        'O',
-        'C',
-        'C+',
-        'A',
-        'A+',
         'P',
-        'A'
-      ]
-    },
-    {
-      'id': 'LSGI013',
-      'name': 'Davis Woodward',
-      'grade': [
-        'A',
-        'B+',
-        'B+',
+        'C',
         'A+',
-        'D+',
-        'C',
-        'C',
-        'C',
         'A+'
-      ]
+      ],
+      'cgpa': 6.5967
     },
     {
-      'id': 'LSGI014',
-      'name': 'Susanna Pruitt',
+      'id': 'SGICS04',
+      'name': 'Hooper Kerr',
+      'grade': [
+        'P',
+        'C',
+        'F',
+        'B+',
+        'B+',
+        'B+',
+        'A+',
+        'C+',
+        'B+'
+      ],
+      'cgpa': 7.272
+    },
+    {
+      'id': 'SGICS05',
+      'name': 'Krista Yates',
       'grade': [
         'F',
-        'C',
-        'B+',
-        'B',
+        'F',
+        'O',
         'A+',
+        'P',
+        'D+',
         'A+',
         'O',
-        'B+',
-        'F'
-      ]
+        'C+'
+      ],
+      'cgpa': 7.5082
     },
     {
-      'id': 'LSGI015',
-      'name': 'Mcmillan Ray',
+      'id': 'SGICS06',
+      'name': 'Mayer Collins',
       'grade': [
+        'A',
         'B',
-        'A+',
-        'A+',
+        'B',
+        'D+',
         'A+',
         'C',
-        'F',
-        'C',
+        'B+',
         'C+',
         'D+'
-      ]
+      ],
+      'cgpa': 9.2091
     },
     {
-      'id': 'LSGI016',
-      'name': 'Terrell Hoover',
+      'id': 'SGICS07',
+      'name': 'Sexton Mason',
       'grade': [
-        'O',
-        'B',
         'F',
-        'A',
-        'B',
-        'A',
-        'C+',
-        'F',
-        'A'
-      ]
-    },
-    {
-      'id': 'LSGI017',
-      'name': 'Cecile Austin',
-      'grade': [
-        'B',
-        'A',
         'P',
+        'B',
+        'C+',
+        'A+',
+        'B+',
+        'D+',
         'O',
+        'F'
+      ],
+      'cgpa': 8.7971
+    },
+    {
+      'id': 'SGICS08',
+      'name': 'Dorothy Salinas',
+      'grade': [
+        'C',
+        'O',
+        'A',
+        'C',
+        'B+',
         'C+',
         'B+',
         'A+',
-        'F',
         'B'
-      ]
+      ],
+      'cgpa': 8.9018
     },
     {
-      'id': 'LSGI018',
-      'name': 'Adela Curtis',
+      'id': 'SGICS09',
+      'name': 'Romero Head',
       'grade': [
-        'D+',
-        'P',
-        'F',
-        'F',
+        'A',
+        'B+',
+        'B',
+        'B+',
+        'B',
         'A+',
-        'C',
-        'C',
-        'P',
+        'B+',
+        'C+',
         'P'
-      ]
+      ],
+      'cgpa': 7.6577
     },
     {
-      'id': 'LSGI019',
-      'name': 'Roy Bates',
-      'grade': [
-        'D+',
-        'B',
-        'B',
-        'P',
-        'O',
-        'D+',
-        'C+',
-        'C+',
-        'A'
-      ]
-    },
-    {
-      'id': 'LSGI020',
-      'name': 'Holt Parks',
-      'grade': [
-        'F',
-        'C',
-        'B',
-        'B',
-        'O',
-        'C',
-        'A',
-        'D+',
-        'F'
-      ]
-    },
-    {
-      'id': 'LSGI021',
-      'name': 'Leticia Lamb',
-      'grade': [
-        'B',
-        'D+',
-        'O',
-        'O',
-        'B',
-        'C+',
-        'D+',
-        'F',
-        'F'
-      ]
-    },
-    {
-      'id': 'LSGI022',
-      'name': 'Marquez Moody',
-      'grade': [
-        'F',
-        'B',
-        'B',
-        'D+',
-        'C',
-        'C+',
-        'P',
-        'A',
-        'A'
-      ]
-    },
-    {
-      'id': 'LSGI023',
-      'name': 'Kristie Carson',
-      'grade': [
-        'A',
-        'C',
-        'D+',
-        'D+',
-        'C+',
-        'C',
-        'P',
-        'O',
-        'F'
-      ]
-    },
-    {
-      'id': 'LSGI024',
-      'name': 'Burnett Salazar',
+      'id': 'SGICS010',
+      'name': 'Odessa Andrews',
       'grade': [
         'P',
+        'C+',
         'A',
-        'C',
-        'B',
-        'A',
-        'F',
+        'B+',
+        'B+',
+        'D+',
+        'D+',
+        'B+',
+        'A+'
+      ],
+      'cgpa': 6.5401
+    },
+    {
+      'id': 'SGICS011',
+      'name': 'Gibson Reese',
+      'grade': [
         'B+',
         'C+',
+        'C+',
+        'C+',
+        'C+',
+        'C+',
+        'O',
+        'O',
+        'C'
+      ],
+      'cgpa': 6.6943
+    },
+    {
+      'id': 'SGICS012',
+      'name': 'Christina Browning',
+      'grade': [
+        'C+',
+        'P',
+        'B',
+        'C',
+        'D+',
+        'P',
+        'F',
+        'B',
+        'C+'
+      ],
+      'cgpa': 6.3839
+    },
+    {
+      'id': 'SGICS013',
+      'name': 'Kaitlin Marsh',
+      'grade': [
+        'B',
+        'C+',
+        'B+',
+        'A+',
+        'C',
+        'O',
+        'O',
+        'D+',
+        'A'
+      ],
+      'cgpa': 7.5293
+    },
+    {
+      'id': 'SGICS014',
+      'name': 'Riggs Harrison',
+      'grade': [
+        'A',
+        'C',
+        'D+',
+        'C',
+        'A',
+        'F',
+        'O',
+        'B+',
+        'B+'
+      ],
+      'cgpa': 7.3946
+    },
+    {
+      'id': 'SGICS015',
+      'name': 'Doreen Holder',
+      'grade': [
+        'F',
+        'C',
+        'F',
+        'O',
+        'A',
+        'D+',
+        'F',
+        'F',
+        'C+'
+      ],
+      'cgpa': 8.3123
+    },
+    {
+      'id': 'SGICS016',
+      'name': 'Wendi Roman',
+      'grade': [
+        'P',
+        'P',
+        'A',
+        'D+',
+        'B+',
+        'D+',
+        'D+',
+        'O',
+        'A'
+      ],
+      'cgpa': 8.3231
+    },
+    {
+      'id': 'SGICS017',
+      'name': 'Buckley Elliott',
+      'grade': [
+        'P',
+        'B',
+        'A',
+        'D+',
+        'A',
+        'P',
+        'P',
+        'C',
+        'A'
+      ],
+      'cgpa': 6.6668
+    },
+    {
+      'id': 'SGICS018',
+      'name': 'Schneider Mcknight',
+      'grade': [
+        'B+',
+        'C',
+        'P',
+        'C+',
+        'C+',
+        'F',
+        'A+',
+        'O',
+        'P'
+      ],
+      'cgpa': 9.238
+    },
+    {
+      'id': 'SGICS019',
+      'name': 'Hudson Pope',
+      'grade': [
+        'P',
+        'A',
+        'F',
+        'A+',
+        'D+',
+        'D+',
+        'O',
+        'D+',
         'D+'
-      ]
+      ],
+      'cgpa': 8.6055
     },
     {
-      'id': 'LSGI025',
-      'name': 'Louisa Fernandez',
+      'id': 'SGICS020',
+      'name': 'Myers Olson',
       'grade': [
+        'C',
+        'A+',
+        'C',
+        'D+',
+        'O',
+        'C',
+        'F',
+        'A+',
+        'F'
+      ],
+      'cgpa': 6.7312
+    },
+    {
+      'id': 'SGICS021',
+      'name': 'Fields Trevino',
+      'grade': [
+        'D+',
+        'C',
+        'B+',
+        'O',
+        'F',
+        'A',
+        'B+',
         'B',
-        'C+',
-        'B',
+        'F'
+      ],
+      'cgpa': 8.8068
+    },
+    {
+      'id': 'SGICS022',
+      'name': 'Sharpe Gregory',
+      'grade': [
         'B',
         'B',
         'B+',
         'B',
+        'O',
+        'O',
+        'D+',
+        'B',
+        'C'
+      ],
+      'cgpa': 8.2972
+    },
+    {
+      'id': 'SGICS023',
+      'name': 'Sonja Garcia',
+      'grade': [
+        'B',
+        'D+',
+        'B+',
+        'C+',
+        'O',
+        'F',
+        'P',
+        'A',
+        'B+'
+      ],
+      'cgpa': 9.6433
+    },
+    {
+      'id': 'SGICS024',
+      'name': 'Mcmillan Mullen',
+      'grade': [
+        'P',
+        'C+',
+        'C+',
+        'F',
+        'P',
         'C',
+        'C',
+        'F',
+        'D+'
+      ],
+      'cgpa': 9.7668
+    },
+    {
+      'id': 'SGICS025',
+      'name': 'Brewer Callahan',
+      'grade': [
+        'C',
+        'C',
+        'C',
+        'F',
+        'C+',
+        'C+',
+        'P',
+        'C',
+        'C+'
+      ],
+      'cgpa': 9.3804
+    },
+    {
+      'id': 'SGICS026',
+      'name': 'Hartman Haney',
+      'grade': [
+        'A+',
+        'B',
+        'B+',
+        'O',
+        'B+',
+        'C+',
+        'B',
+        'B+',
+        'O'
+      ],
+      'cgpa': 7.9556
+    },
+    {
+      'id': 'SGICS027',
+      'name': 'Turner Gallagher',
+      'grade': [
+        'C',
+        'C+',
+        'A',
+        'C',
+        'A',
+        'B+',
+        'A+',
+        'B+',
+        'P'
+      ],
+      'cgpa': 7.6357
+    },
+    {
+      'id': 'SGICS028',
+      'name': 'Jenny Fuller',
+      'grade': [
+        'A+',
+        'D+',
+        'P',
+        'C+',
+        'P',
+        'D+',
+        'O',
+        'A+',
+        'A'
+      ],
+      'cgpa': 9.5947
+    },
+    {
+      'id': 'SGICS029',
+      'name': 'Woodward Mcgee',
+      'grade': [
+        'F',
+        'A',
+        'A+',
+        'A+',
+        'A',
+        'F',
+        'O',
+        'C+',
+        'F'
+      ],
+      'cgpa': 8.1184
+    },
+    {
+      'id': 'SGICS030',
+      'name': 'Levine Adkins',
+      'grade': [
+        'B+',
+        'C+',
+        'D+',
+        'A',
+        'B',
+        'A+',
+        'C+',
+        'B+',
+        'P'
+      ],
+      'cgpa': 6.5973
+    },
+    {
+      'id': 'SGICS031',
+      'name': 'Christensen Powers',
+      'grade': [
+        'P',
+        'B+',
+        'A',
+        'B+',
+        'F',
+        'C',
+        'P',
+        'P',
+        'D+'
+      ],
+      'cgpa': 7.1112
+    },
+    {
+      'id': 'SGICS032',
+      'name': 'Kathleen Kennedy',
+      'grade': [
+        'A+',
+        'O',
+        'O',
+        'C+',
+        'A',
+        'D+',
+        'C+',
+        'B',
         'B'
-      ]
+      ],
+      'cgpa': 8.5148
     },
     {
-      'id': 'LSGI026',
-      'name': 'Jennie Walters',
+      'id': 'SGICS033',
+      'name': 'Freida Houston',
       'grade': [
-        'C+',
+        'O',
         'B+',
         'A+',
-        'C',
-        'A',
-        'A+',
-        'C',
-        'A',
-        'C'
-      ]
-    },
-    {
-      'id': 'LSGI027',
-      'name': 'Arnold Hoffman',
-      'grade': [
         'F',
-        'A',
-        'O',
-        'B+',
-        'B+',
-        'D+',
-        'A+',
-        'A+',
-        'B+'
-      ]
-    },
-    {
-      'id': 'LSGI028',
-      'name': 'Talley Davis',
-      'grade': [
-        'O',
-        'C+',
-        'A+',
-        'B+',
-        'D+',
-        'C',
-        'A',
-        'A+',
-        'C'
-      ]
-    },
-    {
-      'id': 'LSGI029',
-      'name': 'Rene Adams',
-      'grade': [
-        'A+',
-        'O',
-        'O',
-        'O',
-        'A',
         'P',
         'B',
-        'C',
+        'O',
+        'D+',
         'A'
-      ]
+      ],
+      'cgpa': 7.5047
     },
     {
-      'id': 'LSGI030',
-      'name': 'Marisol Sullivan',
+      'id': 'SGICS034',
+      'name': 'Whitney Davidson',
       'grade': [
-        'C+',
+        'A+',
+        'O',
         'C',
-        'C',
-        'B',
-        'A+',
-        'A+',
-        'F',
-        'B',
-        'B+'
-      ]
-    },
-    {
-      'id': 'LSGI031',
-      'name': 'Spears Wilson',
-      'grade': [
+        'O',
+        'A',
         'P',
-        'F',
-        'P',
-        'A+',
+        'A',
         'D+',
-        'F',
+        'O'
+      ],
+      'cgpa': 7.034
+    },
+    {
+      'id': 'SGICS035',
+      'name': 'Agnes Ward',
+      'grade': [
+        'D+',
+        'A+',
+        'P',
+        'B',
+        'B+',
+        'C+',
         'B',
         'A',
-        'A'
-      ]
+        'A+'
+      ],
+      'cgpa': 9.5631
     },
     {
-      'id': 'LSGI032',
-      'name': 'Cecelia Orr',
+      'id': 'SGICS036',
+      'name': 'Ester Simon',
       'grade': [
+        'C+',
+        'B',
+        'F',
+        'B',
         'C',
-        'B',
+        'F',
+        'A',
         'D+',
-        'A+',
-        'A+',
-        'A+',
-        'B+',
-        'B',
         'D+'
-      ]
+      ],
+      'cgpa': 8.8341
+    },
+    {
+      'id': 'SGICS037',
+      'name': 'Chasity Bush',
+      'grade': [
+        'B',
+        'O',
+        'C+',
+        'P',
+        'B+',
+        'B',
+        'C+',
+        'F',
+        'B'
+      ],
+      'cgpa': 9.7765
+    },
+    {
+      'id': 'SGICS038',
+      'name': 'Ryan Herrera',
+      'grade': [
+        'P',
+        'C+',
+        'D+',
+        'D+',
+        'P',
+        'A',
+        'P',
+        'O',
+        'B'
+      ],
+      'cgpa': 7.0699
+    },
+    {
+      'id': 'SGICS039',
+      'name': 'Cline Moreno',
+      'grade': [
+        'C+',
+        'C+',
+        'O',
+        'A+',
+        'A+',
+        'A',
+        'F',
+        'D+',
+        'A+'
+      ],
+      'cgpa': 8.6991
+    },
+    {
+      'id': 'SGICS040',
+      'name': 'Kelli Gross',
+      'grade': [
+        'A',
+        'B+',
+        'F',
+        'B+',
+        'P',
+        'O',
+        'P',
+        'C+',
+        'F'
+      ],
+      'cgpa': 8.6164
+    },
+    {
+      'id': 'SGICS041',
+      'name': 'Coffey Cabrera',
+      'grade': [
+        'C',
+        'B',
+        'P',
+        'A+',
+        'B+',
+        'A',
+        'C',
+        'C',
+        'C'
+      ],
+      'cgpa': 9.4809
     }
   ];
 
