8b005989491c209abc37488a3b3f495460a54532
Raisonjosephau <raisonjosephau@gmail.com>
2018-09-02 13:36:20 +0530

    complete serach services for students

--- src/app/student-result/student-result.componet.ts
+++ src/app/student-result/student-result.componet.ts
@@ -124,6 +124,56 @@ export class StudentResultComponent implements OnInit, AfterViewInit {
       classes: 'input-group-alternative',
       labelKey: 'name'
     };
+    this.results =  [
+      {
+        'id': 0,
+        'code': 'CS031',
+        'name': 'Sreelaksmi Sudeer',
+        'grade': 'B'
+      },
+      {
+        'id': 1,
+        'code': 'CS031',
+        'name': 'Savid Joe Sunny',
+        'grade': 'A+'
+      },
+      {
+        'id': 2,
+        'code': 'CS038',
+        'name': 'Joel Jose Parekattil',
+        'grade': 'F'
+      },
+      {
+        'id': 3,
+        'code': 'CS038',
+        'name': 'Jimenez Cote',
+        'grade': 'B+'
+      },
+      {
+        'id': 4,
+        'code': 'CS034',
+        'name': 'Gonzales Griffith',
+        'grade': 'B'
+      },
+      {
+        'id': 5,
+        'code': 'CS033',
+        'name': 'Kirby Woods',
+        'grade': 'O'
+      },
+      {
+        'id': 6,
+        'code': 'HS031',
+        'name': 'Barry Hinton',
+        'grade': 'O'
+      },
+      {
+        'id': 7,
+        'code': 'CS033',
+        'name': 'Leila Mcfarland',
+        'grade': 'O'
+      }
+    ];
   }
 
   search = (text$: Observable<string>) =>
