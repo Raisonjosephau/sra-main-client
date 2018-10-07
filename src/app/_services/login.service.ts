0cb8a4689918b0f52ebd3aed77adfbccd01c20bc
Raisonjosephau <raisonjosephau@gmail.com>
2018-10-07 07:46:11 +0530

    changes

--- src/app/_services/login.service.ts
+++ src/app/_services/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class LoginService {
@@ -13,12 +13,12 @@ export class LoginService {
       formData.append('username', username);
       formData.append('password', password);
       return this.http
-                  .post<any>('http://127.0.0.1:8000/accounts/login/', formData)
-        .map(data => {
-        // login successful if there's a jwt token in the response
-        if (data.token) {
-            localStorage.setItem('logedUser', JSON.stringify(data));
-        }
+                  .post<any>('http://127.0.0.1:8000/accounts/login/', formData).pipe(
+                    map(data => {
+                    // login successful if there's a jwt token in the response
+                    if (data.token) {
+                        localStorage.setItem('logedUser', JSON.stringify(data));
+                    }
         return data;
       });
   }
--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
-import 'rxjs/add/operator/filter';
+import {filter} from 'rxjs/operators';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
